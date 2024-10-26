c41532bd3396f5879ad25054ecf8de65af4a55f5
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-26 03:20:20 +0300

    DTO For Register

---
+++ LazaProject.Core/DTO'S/RegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Core.DTO_S
+{
+	public class RegisterDTO
+	{
+		public string Name { get; set; }
+		[EmailAddress(ErrorMessage = "Invalid email format.")]
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public bool RememberMe { get; set; }
+	}
+}
