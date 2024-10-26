cf51f68eb6ca2aeb978125f62193817abd665b22
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-26 03:23:27 +0300

    Add Migrations

---
+++ LazaProject.persistence/Migrations/20241024003657_a.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LazaProject.persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class a : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VerificationCode",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VerificationCode",
+                table: "AspNetUsers");
+        }
+    }
+}
