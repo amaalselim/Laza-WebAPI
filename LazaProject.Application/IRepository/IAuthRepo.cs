85eb49ba22b6d500a0f663f88b78b3ab672b3446
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-26 03:18:42 +0300

    Create a Interface for Authorization

---
+++ LazaProject.Application/IRepository/IAuthRepo.cs
@@ -0,0 +1,30 @@
+﻿using LazaProject.Core.DTO_S;
+using LazaProject.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Application.IRepository
+{
+    public interface IAuthRepo
+	{
+		
+
+		Task<IdentityResult> RegisterAsync (RegisterDTO registerDTO);
+		Task<object> LoginAsync (LoginDTO loginDTO);
+		Task<string> GeneratePasswordResetTokenAsync(ForgetPasswordDTO forgetPasswordDTO);
+		Task<IdentityResult> UpdatePasswordAsync(UpdatePasswordDTO updatePasswordDTO);
+		Task SendResetPasswordEmailAsync (string email);	
+		Task SignOutAsync();
+
+
+		Task<object> LoginWithGoogleAsync (GoogleLoginDTO googleLoginDTO);
+		Task<object> LoginWithFacebookAsync (FacebookLoginDTO facebookLoginDTO);
+
+
+
+	}
+}
