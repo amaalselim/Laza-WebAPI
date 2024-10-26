49762f4686ab59ea04a8253e2755cf3655d8039d
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-26 23:26:28 +0300

    Create Assign Role DTO

---
+++ LazaProject.Core/DTO'S/AssignRoleDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LazaProject.Core.Enums;
+
+namespace LazaProject.Core.DTO_S
+{
+    public class AssignRoleDto
+	{
+		[EmailAddress(ErrorMessage = "Invalid email format.")]
+		public string Email { get; set; }
+		public Roles Role { get; set; }
+	}
+}
