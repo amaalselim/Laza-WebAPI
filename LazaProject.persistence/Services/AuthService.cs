a30b734b73b4fad070b918b6e70aa8b54219b0b3
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-26 03:23:18 +0300

    Add External Login in Service

---
+++ LazaProject.persistence/Services/AuthService.cs
@@ -0,0 +1,29 @@
+﻿using LazaProject.Application.IRepository;
+using LazaProject.Core.DTO_S;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.persistence.Services
+{
+	public class AuthService
+	{
+		private readonly IAuthRepo _authRepo;
+
+		public AuthService(IAuthRepo authRepo)
+        {
+			_authRepo = authRepo;
+		}
+		public async Task<object> LoginWithGoogleAsync(GoogleLoginDTO googleLoginDTO)
+		{
+			return await _authRepo.LoginWithGoogleAsync(googleLoginDTO);
+		}
+		public async Task<object> LoginWithFacebookAsync(FacebookLoginDTO facebookLoginDTO)
+		{
+			return await _authRepo.LoginWithFacebookAsync(facebookLoginDTO);
+		}
+
+	}
+}
