ae676b6e21b9184f677b06d0958f6f1ecdc202d9
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-26 03:22:31 +0300

    Create a auto mapper between appuser and registerdto

---
+++ LazaProject.Core/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using LazaProject.Core.DTO_S;
+using LazaProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Core.Mapping
+{
+	public class MappingProfile : Profile
+	{
+        public MappingProfile()
+        {
+            CreateMap<RegisterDTO, ApplicationUser>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email));
+        }
+    }
+}
