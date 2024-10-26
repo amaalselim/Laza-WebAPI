204e89e85fa6bc2e2ce3c0f1003fd02278956f46
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-26 03:19:48 +0300

    add appuser in DbContext

---
+++ LazaProject.persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using LazaProject.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.persistence.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+        public ApplicationDbContext()
+        {
+            
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+		public DbSet<ApplicationUser> applicationUsers{ get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=LazaAPI;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+		}
+	}
+}
