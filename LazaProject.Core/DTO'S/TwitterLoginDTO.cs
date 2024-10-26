cd6383e463341f2b03a501deebec70a6b869d6d2
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-26 03:21:15 +0300

    External Login DTO

---
+++ LazaProject.Core/DTO'S/TwitterLoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Core.DTO_S
+{
+    public class TwitterLoginDTO
+	{
+		public string AccessToken { get; set; }
+		public string TokenSecret { get; set; }
+	}
+}
