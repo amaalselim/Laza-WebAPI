752a345628ade0fff3d1fd49a7c45f6aa7d84595
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-27 18:03:28 +0300

    Changes

--- LazaAPI/Program.cs
+++ LazaAPI/Program.cs
@@ -60,7 +60,7 @@ namespace LazaAPI
 			{
 				corsOptions.AddPolicy("MyPolicy", policyBuilder =>
 				{
-					policyBuilder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+					policyBuilder.WithOrigins("http://localhost:5099").AllowAnyHeader().AllowAnyMethod();
 				});
 			});
 			builder.Services.AddAuthorization(options =>
