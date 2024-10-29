cad65f9137afe4397afe2127324b0ec8d843236d
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-29 22:19:45 +0300

    Make Enum to size and gender

---
+++ LazaProject.Core/Enums/Gender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Core.Enums
+{
+	public enum Gender
+	{
+		Men,
+		Women,
+		Skip
+	}
+}
