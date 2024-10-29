87fb0e4597ec859207cc52e2ac2780b3c438bdcf
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-29 22:21:32 +0300

    Create interface of productimage

---
+++ LazaProject.Application/IRepository/IProductImageRepository.cs
@@ -0,0 +1,14 @@
+﻿using LazaProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Application.IRepository
+{
+    public interface IProductImageRepository : IRepository<productImage>
+    {
+		Task AddRangeAsync(IEnumerable<productImage> productImages);
+	}
+}
