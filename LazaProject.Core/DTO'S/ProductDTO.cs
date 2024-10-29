89a2c6ae3b23481e0ac2c517bf1b901963c8d59c
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-29 22:19:57 +0300

    add productDTo

---
+++ LazaProject.Core/DTO'S/ProductDTO.cs
@@ -0,0 +1,16 @@
+﻿using LazaProject.Core.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+public class ProductDTO
+{
+	public string Name { get; set; }
+	public string Description { get; set; }
+	public string Img { get; set; }
+	public decimal Price { get; set; }
+	[ForeignKey("Category")]
+	public string CategoryId { get; set; }
+	[JsonIgnore]
+	public Category? Category { get; set; }
+	public List<string> Images { get; set; }
+}
