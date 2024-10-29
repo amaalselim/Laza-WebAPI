cb11093527536d0988b3a91bc49147918dadac39
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-29 22:21:13 +0300

    edit

--- LazaProject.Application/IRepository/IRepository.cs
+++ LazaProject.Application/IRepository/IRepository.cs
@@ -8,7 +8,7 @@ namespace LazaProject.Application.IRepository
 {
 	public interface IRepository <T> where T : class
 	{
-		Task<IEnumerable<T>> GetAllAsync ();
+		Task<IEnumerable<T>> GetAllAsync();
 		Task<T> GetByIdAsync (string id);
 		Task AddAsync (T entity);
 		Task UpdateAsync (T entity);
