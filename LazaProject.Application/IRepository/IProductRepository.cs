0dfe0016a5f580b54654db580903c3d040785fb0
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-29 22:21:55 +0300

    create interface of iproduct

---
+++ LazaProject.Application/IRepository/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using LazaProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Application.IRepository
+{
+	public interface IProductRepository : IRepository<Product>
+	{
+		Task<Product> GetImagesByProductIdAsync(string id);
+		Task<IEnumerable<Product>> GetAllImagesByProducts();
+		Task<IEnumerable<Product>> SearchProductAsync (string searchTerm);
+		Task<IEnumerable<Product>> GetAllProductByCategoryIdAsync(string categoryid);
+	}
+}
