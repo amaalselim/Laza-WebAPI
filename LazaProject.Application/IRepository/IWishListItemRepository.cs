c7e279af7c8f9640c51a6b44ede48633347a80a8
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-30 00:14:22 +0300

    Create a interface of Wishlist

---
+++ LazaProject.Application/IRepository/IWishListItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using LazaProject.Core.DTO_S;
+using LazaProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Application.IRepository
+{
+	public interface IWishListItemRepository
+	{
+		Task<bool> AddToWishListAsync(string UserId, string ProductId);
+		Task<bool> RemoveFromWishListAsync(string WishListItemId);
+		Task<IEnumerable<WishListItemDTO>> GetWishListItemByUserIdAsync(string UserId);	
+		Task<IEnumerable<WishListItemDTO>> GetAllWishListAsync();
+
+	}
+}
