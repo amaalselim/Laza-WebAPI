059391ff383d0d42559d1c17e52776c4aa250874
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-30 00:10:11 +0300

    Create a Wishlist Model

---
+++ LazaProject.Core/Models/WishListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LazaProject.Core.Models
+{
+	public class WishListItem
+	{
+		public string Id { get; set; } = Guid.NewGuid().ToString();
+		public string UserId { get; set; }
+		[ForeignKey("Product")]
+		public string ProductId { get; set; }
+		public virtual Product? Product { get; set; }
+	}
+}
