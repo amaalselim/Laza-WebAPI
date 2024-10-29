45633f61e917bdc3c513a4e79be5cb6c0c7b15cb
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-30 00:10:33 +0300

    create wishlist dto

---
+++ LazaProject.Core/DTO'S/WishListItemDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LazaProject.Core.DTO_S
+{
+	public class WishListItemDTO
+	{
+		public string UserId { get; set; }
+		public string ProductId { get; set; }
+		public string? ProductName { get; set; }
+		public decimal? ProductPrice { get; set; }
+
+	}
+}
