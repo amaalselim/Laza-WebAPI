e86e24d29686828312b98ace782088ebc86f99c3
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-30 20:26:28 +0300

    add review dbset

--- LazaProject.persistence/Data/ApplicationDbContext.cs
+++ LazaProject.persistence/Data/ApplicationDbContext.cs
@@ -24,6 +24,7 @@ namespace LazaProject.persistence.Data
 		public DbSet<Product> products{ get; set; }
 		public DbSet<productImage> productImages{ get; set; }
 		public DbSet<WishListItem> wishListItems{ get; set; }
+		public DbSet<Reviews> reviews{ get; set; }
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			//optionsBuilder.UseSqlServer("Server=db9471.public.databaseasp.net; Database=db9471; User Id=db9471; Password=Ed5_9+mDP4=g; Encrypt=False; MultipleActiveResultSets=True;");
