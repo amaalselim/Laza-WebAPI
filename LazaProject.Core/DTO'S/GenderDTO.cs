0857221d2d7144103bf2765deda4f409fecca48c
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-30 22:46:35 +0300

    create genderdto

---
+++ LazaProject.Core/DTO'S/GenderDTO.cs
@@ -0,0 +1,14 @@
+﻿using LazaProject.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Core.DTO_S
+{
+	public class GenderDTO
+	{
+		public Gender Gender { get; set; }
+	}
+}
