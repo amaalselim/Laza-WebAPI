49b002f08dca62dc47743377bdd589c6cefac15f
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-30 20:28:12 +0300

    create IReview Interface

---
+++ LazaProject.Application/IRepository/IReviewRepository.cs
@@ -0,0 +1,17 @@
+﻿using LazaProject.Core.DTO_S;
+using LazaProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Application.IRepository
+{
+    public interface IReviewRepository
+    {
+        Task AddReview(Reviews reviews);
+		Task<IEnumerable<Reviews>> GetReviewsByProductIdAsync(string productId);
+		Task<ReviewSummaryDTO> GetReviewSummaryByProductIdAsync(string productId);
+	}
+}
