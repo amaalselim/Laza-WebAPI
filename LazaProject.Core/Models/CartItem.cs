b26fd05f75311a441cba2619033fb35d2f530e6c
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-31 22:22:34 +0300

    create cartitem model

---
+++ LazaProject.Core/Models/CartItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Core.Models
+{
+	public class CartItem
+	{
+		public string Id { get; set; } = Guid.NewGuid().ToString();
+		public int Quantity	{ get; set; }
+
+		[ForeignKey("Cart")]
+		public string CartId { get; set; }
+		public virtual Cart? Cart { get; set; }
+		[ForeignKey("Product")]
+		public string ProductId { get; set; }
+
+
+		public decimal Price { get; set; }
+		public virtual Product? Product { get; set; }
+		public bool IsActive { get; set; } = true; 
+
+
+
+	}
+}
