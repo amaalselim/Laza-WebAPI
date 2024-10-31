b82e757dbd883f27f2f8d41cfcf110b80424e45c
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-31 22:22:20 +0300

    create cart  model

---
+++ LazaProject.Core/Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Core.Models
+{
+	public class Cart
+	{
+		public string Id { get; set; }= Guid.NewGuid().ToString();
+		public string UserId { get;set; }
+		public List<CartItem> Items { get; set; }= new List<CartItem>();
+		public decimal TotalPrice { get; set; }
+		 
+	}
+}
