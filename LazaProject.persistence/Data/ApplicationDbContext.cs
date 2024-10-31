bbaa98ca81fae8fefb475cd4aa17300c4c8e0e59
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-31 22:22:57 +0300

    add dbset of cart and cartitem

--- LazaProject.persistence/Data/ApplicationDbContext.cs
+++ LazaProject.persistence/Data/ApplicationDbContext.cs
@@ -25,10 +25,12 @@ namespace LazaProject.persistence.Data
 		public DbSet<productImage> productImages{ get; set; }
 		public DbSet<WishListItem> wishListItems{ get; set; }
 		public DbSet<Reviews> reviews{ get; set; }
+		public DbSet<Cart> carts{ get; set; }
+		public DbSet<CartItem> cartItems{ get; set; }
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseSqlServer("Server=db9471.public.databaseasp.net; Database=db9471; User Id=db9471; Password=Ed5_9+mDP4=g; Encrypt=False; MultipleActiveResultSets=True;");
-			//optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=LazaAPI;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+			//optionsBuilder.UseSqlServer("Server=db9584.public.databaseasp.net; Database=db9584; User Id=db9584; Password=8e+XzQ7=4-Tx; Encrypt=False; MultipleActiveResultSets=True;");
+			optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=LazaAPI;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
 		}
 
 	}
