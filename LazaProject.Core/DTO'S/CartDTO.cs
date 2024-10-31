1a756872619aced8d92ce1fcc839c5695c3bf1c1
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-31 22:22:44 +0300

    create a dto's

---
+++ LazaProject.Core/DTO'S/CartDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Core.DTO_S
+{
+	public class CartDTO
+	{
+		public string Id { get; set; }
+		public string UserId { get; set; }
+		public decimal TotalPrice { get; set; }
+		public List<CartItemDTO> Items { get; set; } = new List<CartItemDTO>();
+	}
+}
