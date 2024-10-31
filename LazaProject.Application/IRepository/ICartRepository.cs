57ebf6d21d72c235d2745284e0ae03fc5330ee89
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-31 22:23:12 +0300

    create ICartRepo interface

---
+++ LazaProject.Application/IRepository/ICartRepository.cs
@@ -0,0 +1,19 @@
+﻿using LazaProject.Core.DTO_S;
+using LazaProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Application.IRepository
+{
+	public interface ICartRepository
+	{
+		Task<CartDTO> GetCartAsync (string UserId);
+		Task AddToCartAsync(string  UserId, CartItemDTO cartItemDTO);
+		//Task UpdateCartItemAsync(string  UserId, CartItemDTO cartItemDTO);
+		Task RemoveFromCartAsync (string UserId,string ProductId);
+		Task ClearCartAsync (string UserId);
+	}
+}
