c0c16f0d8de5be39a680e7e99ebe1fb2d7511355
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-31 22:23:58 +0300

    create cartcontroller

---
+++ LazaAPI/Controllers/CartController.cs
@@ -0,0 +1,55 @@
+﻿using Google.Apis.Gmail.v1.Data;
+using LazaProject.Application.IUnitOfWork;
+using LazaProject.Core.DTO_S;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LazaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+		private readonly IUnitOfWork _unitOfWork;
+
+		public CartController(IUnitOfWork unitOfWork)
+        {
+			_unitOfWork = unitOfWork;
+		}
+        [HttpGet("GetCart")]
+        public async Task<ActionResult<CartDTO>> GetCart()
+        {
+            var userId=User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var cart=await _unitOfWork.cartRepository.GetCartAsync(userId);
+            if (cart == null)
+            {
+				return Ok(new { Message = "No items available in the cart." });
+			}
+            return Ok(cart);
+        }
+
+        [HttpPost("AddToCart")]
+        public async Task<IActionResult> AddToCart([FromBody] CartItemDTO cartItemDTO)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            await _unitOfWork.cartRepository.AddToCartAsync(userId,cartItemDTO);
+			return CreatedAtAction(nameof(GetCart), new { userId = userId }, cartItemDTO);
+		}
+		[HttpDelete("Remove-items/{productId}")]
+		public async Task<IActionResult> RemoveFromCart(string productId)
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			await _unitOfWork.cartRepository.RemoveFromCartAsync(userId, productId);
+			return NoContent();
+		}
+		[HttpDelete("Clear-Cart")]
+		public async Task<IActionResult> ClearCart()
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			await _unitOfWork.cartRepository.ClearCartAsync(userId);
+			return NoContent();
+		}
+
+	}
+}
