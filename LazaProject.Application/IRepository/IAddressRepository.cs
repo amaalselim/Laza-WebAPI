1c316bc8a7a3b61bfea533084bcb6f176ddc3892
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-04 19:49:07 +0200

    create an interface of address

---
+++ LazaProject.Application/IRepository/IAddressRepository.cs
@@ -0,0 +1,15 @@
+﻿using LazaProject.Core.DTO_S;
+using LazaProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Application.IRepository
+{
+	public interface IAddressRepository
+	{
+		Task AddAsync (AddressUser addressUser);
+	}
+}
