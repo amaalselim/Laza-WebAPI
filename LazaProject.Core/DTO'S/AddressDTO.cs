3b104b13aad5385dc3b6401cb02d111725101fca
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-04 19:48:04 +0200

    create addressdto

---
+++ LazaProject.Core/DTO'S/AddressDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LazaProject.Core.DTO_S
+{
+	public class AddressDTO
+	{
+		[JsonIgnore]
+		public string? UserId { get; set; }
+		public string UserName { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string Country { get; set; }
+		[Phone(ErrorMessage = "Invalid phone number format.")]
+		public string Phone { get; set; }
+	}
+}
