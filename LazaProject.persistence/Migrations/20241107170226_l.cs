f2d03e9b7eb04ee36721286b7b6d220194503bca
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-07 21:26:32 +0200

    migration

---
+++ LazaProject.persistence/Migrations/20241107170226_l.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LazaProject.persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class l : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                table: "products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
