49da8e036f4eaea407be77d43f0bef97b2143780
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-11-12 21:33:59 +0200

    create card interface and implement a repository

---
+++ LazaProject.Application/IRepository/ICardRepository.cs
@@ -0,0 +1,15 @@
+﻿using LazaProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.Application.IRepository
+{
+	public interface ICardRepository
+	{
+		Task<IEnumerable<Card>> GetAllCardsByUserIdAsync(string UserId);
+		Task AddCardAsync(Card card);
+	}
+}
---
+++ LazaProject.persistence/Repository/CardRepository.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using LazaProject.Application.IRepository;
+using LazaProject.Core.Models;
+using LazaProject.persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazaProject.persistence.Repository
+{
+	public class CardRepository : ICardRepository
+	{
+		private readonly ApplicationDbContext _context;
+
+		public CardRepository(ApplicationDbContext context)
+        {
+			_context = context;
+		}
+		public async Task<IEnumerable<Card>> GetAllCardsByUserIdAsync(string userId)
+		{
+			return await _context.cards.Where(c => c.UserId == userId).ToListAsync();
+		}
+
+		public async Task AddCardAsync(Card card)
+		{
+			await _context.cards.AddAsync(card);
+			await _context.SaveChangesAsync();
+		}
+	}
+}
